e9ce0de3d881c8cf7ef021d04bf01cbda45dd512
Andrew L. Moore <slewsys@gmail.com>
2018-08-05 15:29:33 +0000

    testsuite: Add tests of standard output for display commands.

--- testsuite/l.r
+++ testsuite/l.r
@@ -1,1 +1,11 @@
-
+line 1$
+line 2$
+line 3$
+line 4$
+line5$
+line 2$
+line 4$
+line 1$
+line5$
+line 3$
+5	line5$
--- testsuite/n.r
+++ testsuite/n.r
@@ -1,1 +1,11 @@
-
+1	line 1
+2	line 2
+3	line 3
+4	line 4
+5	line5
+2	line 2
+4	line 4
+1	line 1
+5	line5
+3	line 3$
+5	line5
