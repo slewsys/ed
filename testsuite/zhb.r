4c979aeefbd5bdfc15373064a235b35651c38ad4
Andrew L. Moore <slewsys@gmail.com>
2018-08-06 20:05:11 +0000

    Add missing tests for standard output.

---
+++ testsuite/zhb.r
@@ -0,0 +1,23 @@
+quacum esse (vel augeri & minui) incipiunt & cessant. Extat limes quem
+velocitas in fine motus attingere potest, non autem transgredi. Hæc est
+velocitas ultima. Et par est ratio limitis quantitatum & proportionum
+omnium incipientium & cessantium. Cumq; hic limes sit certus & definitus,
+Problema est vere Geometricum eundem determinare. Geometrica vero omnia in
+aliis Geometricis determinandis ac demonstrandis legitime usurpantur.
+
+Contendi etiam potest, quod si dentur ultimæ quantitatum evanescentium
+rationes, dabuntur & ultimæ magnitudines; & sic quantitas omnis constabit
+ex indivisibilibus, contra quam _Euclides_ de incommensurabilibus, in libro
+decimo Elementorum, demonstravit. Verum hæc Objectio falsæ innititur
+hypothesi. Ultimæ rationes illæ quibuscum quantitates evanescunt, revera
+non sunt rationes quantitatum ultimarum, sed limites ad quos quantitatum
+sine limite decrescentium rationes semper appropinquant, & quas propius
+assequi possunt quam pro data quavis differentia, nunquam vero transgredi,
+neq; prius attingere quam quantitates diminuuntur in infinitum. Res clarius
+intelligetur in infinite magnis. Si quantitates duæ quarum data est
+differentia augeantur in infinitum, dabitur harum ultima ratio, nimirum
+ratio æqualitatis, nec tamen ideo dabuntur quantitates ultimæ seu maximæ
+quarum ista est ratio. Igitur in sequentibus, siquando facili rerum
+imaginationi consulens, dixero quantitates quam minimas, vel evanescentes
+vel ultimas, cave intelligas quantitates magnitudine determinatas, sed
+cogita semper diminuendas sine limite.
---
+++ testsuite/zhf.r
@@ -0,0 +1,23 @@
+DE MOTU CORPORUM
+
+       *       *       *       *       *
+
+Liber PRIMUS
+
+       *       *       *       *       *
+
+SECT. I.
+
+_De Methodo Rationum primarum & ultimarum, cujus ope sequentia
+demonstrantur._
+
+       *       *       *       *       *
+
+LEMMA I.
+
+    _Quantitates, ut & quantitatum rationes, quæ ad æqualitatem dato
+    tempore constanter tendunt & eo pacto propius ad invicem accedere
+    possunt quam pro data quavis differentia; fiunt ultimo æquales._
+
+Si negas, sit earum ultima differentia D. Ergo nequeunt propius ad
+æqualitatem accedere quam pro data differentia D: contra hypothesin.
